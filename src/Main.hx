c859b9e2870834ed0bade14c1780b51eae37d652
Nestor Qin <imba.qxy@gmail.com>
2020-02-02 02:06:01 -0800

    Add MenuState

---
+++ src/Main.hx
@@ -0,0 +1,13 @@
+package;
+
+import states.MenuState;
+import flixel.FlxGame;
+import openfl.display.Sprite;
+import states.PlayState;
+
+class Main extends Sprite {
+	public function new() {
+		super();
+		addChild(new FlxGame(0, 0, MenuState));
+	}
+}
---
+++ src/states/MenuState.hx
@@ -0,0 +1,30 @@
+package states;
+
+import flixel.text.FlxText;
+import flixel.util.FlxAxes;
+import flixel.ui.FlxButton;
+import flixel.FlxState;
+import flixel.FlxG;
+
+class MenuState extends FlxState {
+    var _startBtn: FlxButton;
+    var _title: FlxText;
+
+	override public function create(): Void {
+        super.create();
+		_title = new FlxText(0, 0, 0, "Morphing World", 48);
+		_title.screenCenter();
+		this.add(_title);
+		_startBtn = new FlxButton(0, _title.getScreenPosition().y + 150, "Start", onStart);
+        _startBtn.screenCenter(FlxAxes.X);
+        this.add(_startBtn);
+	}
+
+    override public function update(elapsed: Float): Void {
+        super.update(elapsed);
+    }
+
+    private function onStart(): Void {
+        FlxG.switchState(new PlayState());
+    }
+}
