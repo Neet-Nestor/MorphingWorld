1ed6fb83eaed21661e09edc68e251451ca08d16f
Nestor Qin <imba.qxy@gmail.com>
2020-02-06 15:00:34 -0800

    Set player dimension constant & config in src

--- src/config/Constant.hx
+++ src/config/Constant.hx
@@ -7,6 +7,10 @@ class Constant {
     // Number of frames on the player image per row
     public static inline var playerFramePerRow:Int = 13;
 
+    // Player image dimension in sprite sheet
+    public static inline var playerHeight:Int = 32;
+    public static inline var playerWidth:Int = 32;
+
 	// Physics
 	public static inline var gravity:Float = 820;
 }
