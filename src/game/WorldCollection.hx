e3415ffcad2429926d9b28776e9861c640b529c9
Nestor Qin <imba.qxy@gmail.com>
2020-02-08 17:46:52 -0800

    Delete world2

--- src/game/WorldCollection.hx
+++ src/game/WorldCollection.hx
@@ -12,7 +12,6 @@ class WorldCollection extends NamedCollection<WorldDef> {
 
 	public static function init():Void {
 		defineWorld("world1", false);
-		defineWorld("world2", false);
 	}
 
 	public static function defineWorld(name:String, owned:Bool = false):WorldDef {
