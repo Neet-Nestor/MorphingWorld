a5e80921364d220acc450306b72525f1ba80c942
Nestor Qin <imba.qxy@gmail.com>
2020-02-08 17:04:07 -0800

    Update static extensions

--- src/lycan/util/ext/FlxExt.hx
+++ src/lycan/util/ext/FlxExt.hx
@@ -4,22 +4,23 @@ import flixel.FlxSprite;
 import flixel.FlxObject;
 
 class FlxObjExt {
-	public static function anchorTo(obj:FlxObject, obj2:FlxObject, ?targetX:Float = 0.5, ?targetY:Float = 0.5, anchorX:Float = 0.5, anchorY:Float = 0.5) {
-		if (targetX != null) obj.x = obj2.x + obj2.width * targetX - obj.width * anchorX;
-		if (targetY != null) obj.y = obj2.y + obj2.height * targetY - obj.height * anchorY;
+	public static function anchorTo(obj:FlxObject, obj2:FlxObject, ?targetX:Float = 0.5, ?targetY:Float = 0.5,
+		anchorX:Float = 0.5, anchorY:Float = 0.5):Void {
+			if (targetX != null) obj.x = obj2.x + obj2.width * targetX - obj.width * anchorX;
+			if (targetY != null) obj.y = obj2.y + obj2.height * targetY - obj.height * anchorY;
 	}
 	
 	public static function getCenterX(o:FlxObject):Float {return o.x + o.width / 2;}
 	public static function getCenterY(o:FlxObject):Float {return o.y + o.height / 2;}
-	public static function setCenterX(o:FlxObject, val:Float) {o.x = val - o.width / 2;}
-	public static function setCenterY(o:FlxObject, val:Float) {o.y = val - o.height / 2;}
+	public static function setCenterX(o:FlxObject, val:Float):Void {o.x = val - o.width / 2;}
+	public static function setCenterY(o:FlxObject, val:Float):Void {o.y = val - o.height / 2;}
 	
-	public static function setCenter(o:FlxObject, ?x:Float, ?y:Float) {
+	public static function setCenter(o:FlxObject, ?x:Float, ?y:Float):Void {
 		if (x != null) setCenterX(o, x);
-		if (y != null) setCenterX(y, y);
+		if (y != null) setCenterX(o, y);
 	}
 	
-	public static function scaleTo(spr:FlxSprite, scale:Float, updateHitbox:Bool = true) {
+	public static function scaleTo(spr:FlxSprite, scale:Float, updateHitbox:Bool = true):Void {
 		spr.scale.set(scale, scale);
 		if (updateHitbox) spr.updateHitbox();
 	}
--- src/lycan/world/TiledPropertyExt.hx
+++ src/lycan/world/TiledPropertyExt.hx
@@ -2,30 +2,45 @@ package lycan.world;
 
 import flixel.addons.editors.tiled.TiledPropertySet;
 import flixel.util.helpers.FlxRange;
-using lycan.util.ext.StringExt;
 import flixel.util.FlxColor;
+import lycan.util.ext.StringExt;
 
 class TiledPropertyExt {
-	public static function getBool(props:TiledPropertySet, key:String):Bool {
+	public static function getBool(props:TiledPropertySet, key:String, defaultVal:Bool = false):Bool {
 		var val = props.get(key);
-		return val != null && val.toLowerCase() == "true";
+		if (val != null)
+			return val.toLowerCase() == "true";
+		else
+			return defaultVal;
 	}
 	
-	public static function getInt(props:TiledPropertySet, key:String):Int {
-		return Std.parseInt(props.get(key));
+	public static function getInt(props:TiledPropertySet, key:String, defaultVal:Int = -1):Int {
+		var val = props.get(key);
+		if (val != null)
+			return Std.parseInt(val);
+		else
+			return defaultVal;
 	}
 	
-	public static function getFloat(props:TiledPropertySet, key:String):Float {
-		return Std.parseFloat(props.get(key));
+	public static function getFloat(props:TiledPropertySet, key:String, defaultVal:Int = -1):Float {
+		var val = props.get(key);
+		if (val != null)
+			return Std.parseFloat(val);
+		else
+			return defaultVal;
 	}
 	
 	public static function getRange(props:TiledPropertySet, key:String):{min:Float, max:Float} {
-		return StringExt.parseRange(props.get(key));
+		return (cast props.get(key):String).parseRange();
 	}
-	
-	public static function getColor(props:TiledPropertySet, key:String):FlxColor {
+
+	public static function getColor(props:TiledPropertySet, key:String, defaultVal:FlxColor = FlxColor.WHITE):FlxColor {
 		var val = props.get(key);
-		var a:Array<Int> = haxe.Json.parse(val);
-		return FlxColor.fromRGB(a[0], a[1], a[2]);
+		if (val != null) {
+			var a:Array<Int> = haxe.Json.parse(val);
+			return FlxColor.fromRGB(a[0], a[1], a[2]);
+		} else {
+			return defaultVal;
+		}
 	}
 }
