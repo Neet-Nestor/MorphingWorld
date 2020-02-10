ed2664e8c91cd8b9b0239a2acd2dcbe7dd498214
Andrew Liu <andrewliu@Andrews-MacBook-Pro.local>
2020-02-10 00:50:20 -0800

    add L2 map

---
+++ assets/data/portalRings1.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="portalRings1" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="../images/portalRings1.png" width="128" height="160"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
