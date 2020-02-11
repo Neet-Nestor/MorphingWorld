55e09270ca8e93b9e9636545b700b710be205014
Nestor Qin <imba.qxy@gmail.com>
2020-02-11 01:08:54 -0800

    Chest animation

--- src/sprites/WorldPiece.hx
+++ src/sprites/WorldPiece.hx
@@ -30,12 +30,11 @@ class WorldPiece extends LSprite implements PhysicsEntity implements Collectable
 	public function new() {
 		super();
 		
-        // loadGraphic("assets/images/worldpiece.png", true, 25, 26, false);
-		// animation.add("spinning", [0, 1, 2, 3, 4, 5], 10);
-		// animation.play("spinning");
-        makeGraphic(32, 32, FlxColor.WHITE);
-		physics.init(BodyType.KINEMATIC, false, false);
-		physics.createRectangularBody(32, 32, BodyType.KINEMATIC);
+        loadGraphic("assets/images/chest.png", true, 32, 32, false);
+		animation.add("open", [0], 0);
+		animation.add("close", [1], 0);
+		animation.play("close");
+		physics.init(BodyType.KINEMATIC, true, false);
 		physics.body.userData.entity = this;
 		physics.enabled = true;
 		physics.body.shapes.at(0).sensorEnabled = true;
@@ -46,12 +45,7 @@ class WorldPiece extends LSprite implements PhysicsEntity implements Collectable
 			var player:Player = cast c;
 			physics.enabled = false;
 			physics.enableUpdate = false;
-			moves = true;
-			velocity.y = -250;
-			acceleration.y = 550;
-			// animation.curAnim.frameRate = 30;
-			vanishTween = FlxTween.tween(this, {alpha: 0}, 0.7, {ease: FlxEase.quadIn});
-			scaleTween = FlxTween.tween(this.scale, {x: 0, y: 0}, 0.7, {ease: FlxEase.quadIn});
+			animation.play("open");
 			if (!worldDef.owned) {
 				worldDef.owned = true;
 				new FlxTimer().start(0.22, (_) -> {PlayState.instance.collectWorld(worldDef);});
