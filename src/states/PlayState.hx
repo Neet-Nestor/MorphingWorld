edd2fa0d20d4511c7e80ecc5ae72e44f2b070225
Nestor Qin <imba.qxy@gmail.com>
2020-02-11 02:44:57 -0800

    show text duration deleted

--- src/states/PlayState.hx
+++ src/states/PlayState.hx
@@ -334,7 +334,7 @@ class PlayState extends LycanState {
     // Helper functions
 
     // Create a text on screen
-	public function showText(str:String, showTime:Float = 1.65, duration:Float = 10, ?group:FlxSpriteGroup):Void {
+	public function showText(str:String, showTime:Float = 1.65, ?group:FlxSpriteGroup):Void {
 		var t = new FlxText(0, 0, 0, str, 20);
 		// t.font = "fairfax";
 		t.y = FlxG.height - 50;
@@ -344,12 +344,10 @@ class PlayState extends LycanState {
         if (group == null) group = uiGroup;
 
 		FlxTween.tween(t, {alpha: 1}, 0.6, {onComplete: (_) -> {
-            new FlxTimer(timers).start(duration, (_) -> {
-                FlxTween.tween(t, {alpha: 0}, 0.6, {startDelay: showTime, onComplete: (_) -> {
-                    group.remove(t);
-                    t.destroy();
-                }});
-            });
+            FlxTween.tween(t, {alpha: 0}, 0.6, {startDelay: showTime, onComplete: (_) -> {
+                group.remove(t);
+                t.destroy();
+            }});
 		}});
 
 		group.add(t);
