044a6f1811c04daeabba466c766e7c540a81ba1f
Andrew Liu <andrewliu@Andrews-MacBook-Pro.local>
2020-02-11 00:44:22 -0800

    add background

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -5,6 +5,14 @@ import flixel.util.FlxAxes;
 import flixel.ui.FlxButton;
 import flixel.FlxG;
 import lycan.states.LycanState;
+import flixel.FlxSprite;
+import flixel.addons.ui.FlxUI;
+import flixel.addons.ui.FlxUI9SliceSprite;
+import flixel.addons.ui.FlxUIButton;
+import flixel.addons.ui.FlxUICheckBox;
+import flixel.addons.ui.FlxUIRadioGroup;
+import flixel.addons.ui.FlxUITabMenu;
+import flixel.addons.ui.FlxUIText;
 
 class MenuState extends LycanState {
     private var startBtn:FlxButton;
@@ -14,6 +22,14 @@ class MenuState extends LycanState {
         super.create();
         title = new FlxText(0, 0, 0, "Morphing World", 48);
         title.screenCenter();
+        // var back = new FlxSprite(0, 0, "assets/images/menubg.jpg");
+        // back.resize(FlxG.stage.stageWidth, FlxG.stage.stageHeight);
+        // back.scale.set(0.5, 0.5);
+        var back = new FlxSprite();
+        back.loadGraphic("assets/images/menubg.jpg");
+        back.x = 0;
+        back.y = 0;
+        add(back);
         add(title);
         startBtn = new FlxButton(0, title.getScreenPosition().y + 150, "Start", onStart);
         startBtn.screenCenter(FlxAxes.X);
