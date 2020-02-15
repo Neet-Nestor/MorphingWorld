bf552bffb243643524fb8af53f6585deb19ac896
Hongxiao Lyu <lyuh@uw.edu>
2020-02-14 23:49:31 -0800

    fix

---
+++ assets/data/Sprute.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Sprute" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="../images/Sprute.png" width="224" height="224"/>
+</tileset>
---
+++ assets/data/portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="portal" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="../images/portal.png" width="128" height="160"/>
+</tileset>
