fe2d63e1775619c28b509f30a755e3ca79de134e
Hongxiao Lyu <lyuh@uw.edu>
2020-02-14 23:46:51 -0800

    try

--- src/states/MenuState.hx
+++ src/states/MenuState.hx
@@ -14,6 +14,7 @@ import flixel.addons.ui.FlxUIRadioGroup;
 import flixel.addons.ui.FlxUITabMenu;
 import flixel.addons.ui.FlxUIText;
 
+
 class MenuState extends LycanState {
     private var startBtn:FlxButton;
     private var title:FlxText;
