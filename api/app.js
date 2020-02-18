5bfaa42cd9aeeee56e1f59579907404c3042b713
Nestor Qin <imba.qxy@gmail.com>
2020-02-17 18:23:32 -0800

    Add Eslint

--- api/app.js
+++ api/app.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const redis = require("redis");
+const winston = require("winston");
+const expressWinston = require("express-winston");
 const { redisConfig } = require("./config");
 
 const client = redis.createClient(redisConfig);
 const app = express();
-
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  apps : [
-      {
-      name: 'MW Redis API',
-      script: 'app.js',
+    apps : [
+        {
+            name: "MW Redis API",
+            script: "app.js",
 
-      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-      instances: 'max',
-      autorestart: true,
-      watch: true,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development'
-      },
-      env_production: {
-        NODE_ENV: 'production'
-      }
-    }
-  ],
+            // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+            instances: "max",
+            autorestart: true,
+            watch: true,
+            max_memory_restart: "1G",
+            env: {
+                NODE_ENV: "development"
+            },
+            env_production: {
+                NODE_ENV: "production"
+            }
+        }
+    ],
 };
