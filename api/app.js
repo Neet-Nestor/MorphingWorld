2bde2c21eb50797355e0c1e22ed579a6555b7c78
Nestor Qin <imba.qxy@gmail.com>
2020-02-17 18:24:45 -0800

    User module router

--- api/app.js
+++ api/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const redis = require("redis");
 const winston = require("winston");
 const expressWinston = require("express-winston");
-const { redisConfig } = require("./config");
+const router = require("./router");
 
-const client = redis.createClient(redisConfig);
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
+app.use(router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
