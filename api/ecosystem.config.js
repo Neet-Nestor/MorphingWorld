db2144c4711377997070a939b56808e94dbcae8a
Nestor Qin <imba.qxy@gmail.com>
2020-02-17 18:17:10 -0800

    Add pm2 management

---
+++ api/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [
+      {
+      name: 'MW Redis API',
+      script: 'app.js',
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 'max',
+      autorestart: true,
+      watch: true,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+};
