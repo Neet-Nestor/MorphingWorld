8e7f175d9690354cbf4cc57a473551d5b0bbae61
Nestor Qin <imba.qxy@gmail.com>
2020-02-25 10:37:22 -0800

    Add some printing

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,3 +1,8 @@
+echo "Preparing zip...\n"
 zip api.zip app.js config.js ecosystem.config.js log.js package-lock.json package.json query.js
+echo "\n"
+echo "Uploading the zip...\n"
 scp api.zip vultr:~/mwlog/
+echo "Unziping the zip on target machine...\n"
 ssh vultr "cd ~/mwlog && unzip -o api.zip"
+echo "Deployed\n"
