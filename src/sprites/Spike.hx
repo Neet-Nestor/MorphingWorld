ed222b9e8e7f621771a6907451265355784bc748
Nestor Qin <imba.qxy@gmail.com>
2020-03-06 17:41:21 -0800

    Fix wrong spike problem

--- src/sprites/Spike.hx
+++ src/sprites/Spike.hx
@@ -26,8 +26,8 @@ class Spike extends DamagerSprite {
 	public function new(x:Float = 0, y:Float = 0) {
 		super(BodyType.KINEMATIC);
 		
-		this.setCenter(x, y);
 		this.loadGraphic(AssetPaths.spike__png);
+		this.setCenter(x, y); // This must be after loading Graphic
 		physics.createBodyFromBitmap(pixels, 0x08, BodyType.STATIC);
 		tweens = new FlxTweenManager();
 		
