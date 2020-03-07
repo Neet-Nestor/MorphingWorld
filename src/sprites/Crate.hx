391fbb7c310f65859803dfb9a0d7cdfec017d132
Nestor Qin <imba.qxy@gmail.com>
2020-03-07 01:46:38 -0800

    CRATE to round base

--- src/sprites/Crate.hx
+++ src/sprites/Crate.hx
@@ -1,5 +1,6 @@
 package sprites;
 
+import config.Config;
 import flixel.system.debug.watch.Tracker.TrackerProfile;
 import nape.phys.Material;
 import flixel.FlxG;
@@ -11,6 +12,8 @@ import nape.callbacks.CbType;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import nape.phys.BodyType;
+import nape.shape.Polygon;
+import nape.dynamics.InteractionGroup;
 import lycan.util.GraphicUtil;
 import lycan.world.components.PhysicsEntity;
 import lycan.game3D.components.Physics3D;
@@ -18,6 +21,7 @@ import lycan.entities.LSprite;
 import lycan.world.components.Groundable;
 
 class Crate extends LSprite implements PhysicsEntity implements Groundable {
+	public static var CRATE_SHAPES_GROUP:InteractionGroup = new InteractionGroup(true);
 	public static var PUSHABLE_TYPE:CbType = new CbType();
 	public static var CRATE_TYPE:CbType = new CbType();
 	public static var CRATE_MATERIAL:Material = new Material(0, 0.3, 0.05, 0.3);
@@ -27,12 +31,19 @@ class Crate extends LSprite implements PhysicsEntity implements Groundable {
 
 		loadGraphic(AssetPaths.crate__png);
 
-		physics.init(BodyType.DYNAMIC);
+		physics.init(BodyType.DYNAMIC, false);
+		physics.createCircularBody(width / 2, BodyType.DYNAMIC);
+		physics.body.shapes.add(new Polygon(Polygon.rect(-width / 2, -height / 2, width, height / 2)));
+		for (shape in physics.body.shapes) {
+			shape.group = CRATE_SHAPES_GROUP;
+		}
+
 		physics.body.allowRotation = false;
 		physics.body.setShapeMaterials(CRATE_MATERIAL);
 		physics.body.cbTypes.add(PUSHABLE_TYPE);
 		physics.body.cbTypes.add(CRATE_TYPE);
 		physics.body.cbTypes.add(PlatformerPhysics.GROUNDABLE_TYPE);
+
 	}
 	
 	override public function update(elapsed:Float):Void {
